d655847153ad14f3fae7ca26f52cb4938a43ffaf
Jibrugo <97877391+Jibrugo@users.noreply.github.com>
2024-03-18 15:03:55 +0100

    Le jeu fini

--- app/src/main/java/com/example/testomg/EndActivity.java
+++ app/src/main/java/com/example/testomg/EndActivity.java
@@ -1,8 +1,12 @@
 package com.example.testomg;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -11,20 +15,37 @@ public class EndActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_end);
 
         // Récupérer les données passées avec l'intent
         Intent intent = getIntent();
-        int nombreEssais = intent.getIntExtra("nombreEssais", 0);
-        String motATrouver = intent.getStringExtra("motATrouver");
-        long temps = intent.getLongExtra("temps", 0);
+        String[] tableauDeString = intent.getStringArrayExtra("listMots");
+        int[] tableauDeInt = intent.getIntArrayExtra("listValidOrNot");
+        int nombreEssais = intent.getIntExtra("nombreEssaisTotaux", 0);
 
         // Afficher les données dans les TextView de votre mise en page
         TextView essaisTextView = findViewById(R.id.essaisTextView);
         essaisTextView.setText("Nombre d'essais : " + nombreEssais);
 
-        TextView motATrouverTextView = findViewById(R.id.motATrouverTextView);
-        motATrouverTextView.setText("Mot à trouver : " + motATrouver);
+        // Récupérer le conteneur pour les mots à afficher
+        LinearLayout motATrouverContainer = findViewById(R.id.motATrouverContainer);
+
+        // Pour chaque élément du tableau, créer un nouveau TextView et l'ajouter au conteneur
+        for (int i = 0; i < tableauDeInt.length; i++) {
+            TextView nouveauTextView = new TextView(this);
+            nouveauTextView.setText(tableauDeString[i]);
+
+            // Définissez la couleur de fond du TextView en fonction de la valeur dans le tableau d'entiers
+            if (tableauDeInt[i] == 1) {
+                nouveauTextView.setBackgroundColor(Color.GREEN);
+            } else {
+                nouveauTextView.setBackgroundColor(Color.RED);
+            }
+
+            // Ajouter le TextView au conteneur
+            motATrouverContainer.addView(nouveauTextView);
+        }
 
     }
 }
--- app/src/main/java/com/example/testomg/MainActivity.java
+++ app/src/main/java/com/example/testomg/MainActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import java.util.Random;
+
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -140,104 +144,308 @@ public class MainActivity extends AppCompatActivity {
 
             // Ajoutez le TextView au layout
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT
+                    100, // Largeur de la vue vide (vous pouvez ajuster selon vos besoins)
+                    ViewGroup.LayoutParams.MATCH_PARENT // Hauteur de la vue vide (correspond à celle du conteneur)
             );
-            params.setMargins(10, 10, 10, 10); // Ajustez les marges selon vos besoins
+            params.setMargins(10, 3, 10, 3); // Ajustez les marges selon vos besoins
             lettreTextView.setLayoutParams(params);
 
             layoute.addView(lettreTextView);
         }
     }
 
-    private void victoire(int cpt, String mot){
+    private String motmodif(String motPropose,String motMystere){
+
+        char[] tableauDeChar = motPropose.toCharArray();
+        char[] tableauDeReponse = motMystere.toCharArray();
+        for(int i = 0; i < motPropose.length(); i++) {
+            if ( tableauDeChar[i] != tableauDeReponse[i]) {
+                tableauDeChar[i] = ' ';
+            }
+        }
+        String motmodifier = new String(tableauDeChar);
+        return motmodifier;
+    }
+
+    private void victoire(int cptotaux, String[] mots, int[] yesOrNo){
         Intent intent = new Intent(MainActivity.this, EndActivity.class);
-        intent.putExtra("nombreEssais", cpt); // Remplacez nombreEssais par la variable réelle
-        intent.putExtra("motATrouver", mot); // Remplacez motATrouver par la variable réelle
+        intent.putExtra("nombreEssaisTotaux", cptotaux);
+        intent.putExtra("listMots", mots); //
+        intent.putExtra("listValidOrNot", yesOrNo);
         startActivity(intent);
+
     }
     private void defaite(String mot){
-        Intent intent = new Intent(MainActivity.this, Defeat.class);
+        Intent intent = new Intent(MainActivity.this, EndActivity.class);
         intent.putExtra("motATrouver", mot); // Remplacez motATrouver par la variable réelle
         startActivity(intent);
     }
+
+    private void afficherCasesVides(LinearLayout layout, int nombreLettresAttendues) {
+        layout.removeAllViews(); // Effacer les vues précédentes
+
+        // Ajouter des vues vides pour représenter les cases vides
+        for (int i = 0; i < nombreLettresAttendues; i++) {
+            TextView emptyTextView = new TextView(this);
+            emptyTextView.setText(" "); // Caractère spécial vide
+            emptyTextView.setBackgroundColor(Color.GRAY); // Fond gris
+            emptyTextView.setGravity(Gravity.CENTER); // Centrer le texte
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    100, // Largeur de la vue vide (vous pouvez ajuster selon vos besoins)
+                    ViewGroup.LayoutParams.MATCH_PARENT // Hauteur de la vue vide (correspond à celle du conteneur)
+            );
+            params.setMargins(10, 3, 10, 3); // Ajuster les marges selon vos besoins
+            emptyTextView.setLayoutParams(params);
+            layout.addView(emptyTextView);
+        }
+    }
+
+    // mode déterministe
+    private String[] deterministeMots(int[] longueurs){
+        String[] mots = new String[longueurs.length];
+
+        for (int i = 0; i < longueurs.length; i++) {
+            mots[i] = selectRandom(longueurs[i]);
+
+        }
+        return mots;
+    }
+
+    // mode random
+    private String[] randomMots(){
+        // mot mystère
+        Random random = new Random();
+        // Obtenez un nombre aléatoire entre 2 (inclus) et 10 (exclus)
+        String[] mots = {selectRandom(random.nextInt(8) + 2),selectRandom(random.nextInt(8) + 2),selectRandom(random.nextInt(8) + 2),selectRandom(random.nextInt(8) + 2),selectRandom(random.nextInt(8) + 2)};
+        return mots;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
 
-        LinearLayout  propo1 = findViewById(R.id.Proposition1);
-        LinearLayout  propo2 = findViewById(R.id.Proposition2);
-        LinearLayout  propo3 = findViewById(R.id.Proposition3);
-        LinearLayout  propo4 = findViewById(R.id.Proposition4);
-        LinearLayout  propo5 = findViewById(R.id.Proposition5);
-        LinearLayout  propo6 = findViewById(R.id.Proposition6);
+        LinearLayout propo1 = findViewById(R.id.Proposition1);
+        LinearLayout propo2 = findViewById(R.id.Proposition2);
+        LinearLayout propo3 = findViewById(R.id.Proposition3);
+        LinearLayout propo4 = findViewById(R.id.Proposition4);
+        LinearLayout propo5 = findViewById(R.id.Proposition5);
+        LinearLayout propo6 = findViewById(R.id.Proposition6);
         EditText editText = findViewById(R.id.EntreeMots);
-        // mot mystère
-        Random random = new Random();
+        String mode = "random";
 
-        // Obtenez un nombre aléatoire entre 2 (inclus) et 10 (exclus)
-        int nombreAleatoire = random.nextInt(8) + 2;
-        String mot = selectRandom(nombreAleatoire);
+        final String[] mot;
+        if (mode.equals("random")) {
+            mot = randomMots();
+        } else{
+            mot = randomMots();
+        }
+
+        final int[] indicePartie = {0};
+
+
         // faire fonction de check in des lettres pour afficher en conséquence
         // du toUpperCase
         // Faire Char par char et attribué une value en mode matrice du mot -> couleur selon 0 1 2
-        
 
+        afficherCasesVides(propo1, mot[indicePartie[0]].length());
+        afficherCasesVides(propo2, mot[indicePartie[0]].length());
+        afficherCasesVides(propo3, mot[indicePartie[0]].length());
+        afficherCasesVides(propo4, mot[indicePartie[0]].length());
+        afficherCasesVides(propo5, mot[indicePartie[0]].length());
+        afficherCasesVides(propo6, mot[indicePartie[0]].length());
         // faudra limiter selon le mot de la partie
-        int maxLength = mot.length();
-        editText.setFilters(new InputFilter[]{new InputFilter.AllCaps(),new InputFilter.LengthFilter(maxLength)});
+        final int[] maxLength = {mot[indicePartie[0]].length()};
+        editText.setFilters(new InputFilter[]{new InputFilter.AllCaps(),new InputFilter.LengthFilter(maxLength[0])});
 
+
+
+
+        // Nombre mots partie et les trouvés.
+        final int[] motsPartie = {0};
+        final int[] motsTotal = {5};
+        final int[] motsDecouverts = {0};
+        final int[] tableauDeInt = {0, 0, 0, 0, 0};
+        final String[] tableauDeMots = {"","","","",""};
+        tableauDeMots[indicePartie[0]] = mot[indicePartie[0]];
         // Compteur de la proposition combien on est pour switch
         final int[] cpt = {1};
+        final int[] cptotaux = {0};
+
+
+        Button motSuivantButton = findViewById(R.id.motSuivantButton);
+        motSuivantButton.setVisibility(View.INVISIBLE); // Masquer le bouton au début
+
         Button button = findViewById(R.id.Propose);
         button.setOnClickListener(v -> {
-            if(editText.getText().toString().length()== mot.length() && rechercherMotDansFichier(editText.getText().toString())) {
+            if(editText.getText().toString().length()== mot[indicePartie[0]].length() && rechercherMotDansFichier(editText.getText().toString())) {
                 switch (cpt[0]) {
 
                     case 1:
-                        afficherLettresAvecCouleur(propo1, editText.getText().toString(), mot);
+                        afficherLettresAvecCouleur(propo1, editText.getText().toString(), mot[indicePartie[0]]);
+                        afficherLettresAvecCouleur(propo2, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
                         cpt[0]++;
                         break;
 
                     case 2:
-                        afficherLettresAvecCouleur(propo2, editText.getText().toString(), mot);
+                        afficherLettresAvecCouleur(propo2, editText.getText().toString(), mot[indicePartie[0]]);
+                        afficherLettresAvecCouleur(propo3, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
                         cpt[0]++;
                         break;
 
                     case 3:
-                        afficherLettresAvecCouleur(propo3, editText.getText().toString(), mot);
+                        afficherLettresAvecCouleur(propo3, editText.getText().toString(), mot[indicePartie[0]]);
+                        afficherLettresAvecCouleur(propo4, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
                         cpt[0]++;
                         break;
                     case 4:
-                        afficherLettresAvecCouleur(propo4, editText.getText().toString(), mot);
+                        afficherLettresAvecCouleur(propo4, editText.getText().toString(), mot[indicePartie[0]]);
+                        afficherLettresAvecCouleur(propo5, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
                         cpt[0]++;
                         break;
                     case 5:
-                        afficherLettresAvecCouleur(propo5, editText.getText().toString(), mot);
+                        afficherLettresAvecCouleur(propo5, editText.getText().toString(), mot[indicePartie[0]]);
+                        afficherLettresAvecCouleur(propo6, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
                         cpt[0]++;
                         break;
                     case 6:
-                        afficherLettresAvecCouleur(propo6, editText.getText().toString(), mot);
+                        afficherLettresAvecCouleur(propo6, editText.getText().toString(), mot[indicePartie[0]]);
                         cpt[0]++;
                         break;
                     default:
                         break;
                 }
-                if(editText.getText().toString().equals(mot) && cpt[0] <= 7){
-                    victoire(cpt[0],mot);
+                if(editText.getText().toString().equals(mot[indicePartie[0]]) && cpt[indicePartie[0]] <= 7){
+
+                    motsDecouverts[0]++;
+                    if (motsDecouverts[0] < motsTotal[0]) {
+                        motSuivantButton.setVisibility(View.VISIBLE);
+                    }
+                    tableauDeInt[motsPartie[0]] = 1;
+                    tableauDeMots[motsPartie[0]] = mot[indicePartie[0]];
+
+                    motsPartie[0]++;
                     button.setEnabled(false);
                     cpt[0] = 8;
-                } else if ((!editText.getText().toString().equals(mot)) && cpt[0] == 7) {
-                    defaite(mot);
+                } else if ((!editText.getText().toString().equals(mot[indicePartie[0]])) && cpt[0] == 7) {
+                    tableauDeMots[motsPartie[0]] = mot[indicePartie[0]];
+                    motsPartie[0]++;
                     button.setEnabled(false);
                     cpt[0] = 8;
+                    if (motsDecouverts[0] < motsTotal[0]) {
+                        motSuivantButton.setVisibility(View.VISIBLE);
+                    }
+                }
+                if (motsPartie[0] == motsTotal[0]) {
+                    victoire(cptotaux[0],tableauDeMots,tableauDeInt );
                 }
                 editText.setText((""));
             }
         });
 
+        editText.setOnKeyListener((view, keyCode, keyEvent) -> {
+            if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && ( keyCode == KeyEvent.KEYCODE_ENTER)) {
+                if(editText.getText().toString().length()== mot[indicePartie[0]].length() && rechercherMotDansFichier(editText.getText().toString())) {
+                    switch (cpt[0]) {
+                        case 1:
+                            afficherLettresAvecCouleur(propo1, editText.getText().toString(), mot[indicePartie[0]]);
+                            afficherLettresAvecCouleur(propo2, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
+                            cpt[0]++;
+                            break;
+                        case 2:
+                            afficherLettresAvecCouleur(propo2, editText.getText().toString(), mot[indicePartie[0]]);
+                            afficherLettresAvecCouleur(propo3, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
+                            cpt[0]++;
+                            break;
+                        case 3:
+                            afficherLettresAvecCouleur(propo3, editText.getText().toString(), mot[indicePartie[0]]);
+                            afficherLettresAvecCouleur(propo4, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
+                            cpt[0]++;
+                            break;
+                        case 4:
+                            afficherLettresAvecCouleur(propo4, editText.getText().toString(), mot[indicePartie[0]]);
+                            afficherLettresAvecCouleur(propo5, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
+                            cpt[0]++;
+                            break;
+                        case 5:
+                            afficherLettresAvecCouleur(propo5, editText.getText().toString(), mot[indicePartie[0]]);
+                            afficherLettresAvecCouleur(propo6, motmodif(editText.getText().toString(),mot[indicePartie[0]]), mot[indicePartie[0]]);
+                            cpt[0]++;
+                            break;
+                        case 6:
+                            afficherLettresAvecCouleur(propo6, editText.getText().toString(), mot[indicePartie[0]]);
+                            cpt[0]++;
+                            break;
+                        default:
+                            break;
+                    }
+                    if(editText.getText().toString().equals(mot[indicePartie[0]]) && cpt[0] <= 7){
+                        motsDecouverts[0]++;
+                        if (motsDecouverts[0] < motsTotal[0]) {
+                            motSuivantButton.setVisibility(View.VISIBLE);
+                        }
+                        tableauDeInt[motsPartie[0]] = 1;
+                        tableauDeMots[motsPartie[0]] = mot[indicePartie[0]];
+                        motsPartie[0]++;
+                        button.setEnabled(false);
+                        cpt[0] = 8;
+                    } else if ((!editText.getText().toString().equals(mot[indicePartie[0]])) && cpt[0] == 7) {
+                        tableauDeMots[motsPartie[0]] = mot[indicePartie[0]];
+                        motsPartie[0]++;
+                        button.setEnabled(false);
+                        cpt[0] = 8;
+                        if (motsDecouverts[0] < motsTotal[0]) {
+                            motSuivantButton.setVisibility(View.VISIBLE);
+                        }
+                    }
 
+                    if (motsPartie[0] == motsTotal[0]) {
+                        victoire(cptotaux[0],tableauDeMots,tableauDeInt );
+                    }
+                    editText.setText((""));
+                }
+                return true; // Indique que l'événement a été géré
+            }
+            return false; // Indique que l'événement n'a pas été géré
+        });
+
+        motSuivantButton.setOnClickListener(v -> {
+            // Générer un nouveau mot mystère
+
+            indicePartie[0] += 1;
+            // Réinitialiser l'interface pour le nouveau mot
+            // Effacer les vues des propositions précédentes
+            propo1.removeAllViews();
+            propo2.removeAllViews();
+            propo3.removeAllViews();
+            propo4.removeAllViews();
+            propo5.removeAllViews();
+            propo6.removeAllViews();
+
+            afficherCasesVides(propo1, mot[indicePartie[0]].length());
+            afficherCasesVides(propo2, mot[indicePartie[0]].length());
+            afficherCasesVides(propo3, mot[indicePartie[0]].length());
+            afficherCasesVides(propo4, mot[indicePartie[0]].length());
+            afficherCasesVides(propo5, mot[indicePartie[0]].length());
+            afficherCasesVides(propo6, mot[indicePartie[0]].length());
+            maxLength[0] = mot[indicePartie[0]].length();
+            editText.setFilters(new InputFilter[]{new InputFilter.AllCaps(),new InputFilter.LengthFilter(maxLength[0])});
+            cptotaux[0] += cpt[0];
+            // Réinitialiser le compteur de propositions
+            cpt[0] = 1;
+
+            // Réinitialiser le champ de saisie
+            editText.setText("");
+
+            // Réinitialiser le bouton "Proposer"
+            button.setEnabled(true);
+
+            // Masquer le bouton "Mot Suivant"
+            motSuivantButton.setVisibility(View.INVISIBLE);
+            // Logique pour passer au mot suivant
+        });
     }
 }
