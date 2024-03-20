668eb6de41ed88bc2f4a193ec3a73b414f394fc6
Jibrugo <97877391+Jibrugo@users.noreply.github.com>
2024-03-20 16:00:03 +0100

    Page compte

---
+++ app/src/main/java/com/example/myapplication/compteuh.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class compteuh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.compte);
+
+        //// Lit dans la BDD pseudo, le nb de niveaux fini, le nb de mots devinés et le nb d'essais au total
+    }
+}
