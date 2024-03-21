82916b19445758d887dd5f730e60b5dbf8a42683
Elias <elias.okat.etu@univ-lemans.fr>
2024-03-21 17:06:26 +0100

    Interface pour le fichier Json

---
+++ app/src/main/java/com/example/myapplication/JsonObject.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+public class JsonObject {
+    private String words;
+    private int size;
+
+    public String getWords() {
+        return words;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
